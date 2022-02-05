5fe3b181d94523e135fbfbba227a22b0b6da7477
Gonzalo Marcos Vaquero <gmarcosvaquero@gmail.com>
2022-02-05 21:16:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require('fs');
-const readline = require('readline');
-const { google } = require('googleapis');
 
 client.on('ready', () => {
     console.log(`Andamo ruleta`); // wea on bot ready
