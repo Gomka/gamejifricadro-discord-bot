bc97449b5e285ef177881c6367d32936616174ce
Gonzalo Marcos Vaquero <gmarcosvaquero@gmail.com>
2022-02-05 19:57:43 +0100

    no more self loops yupi

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ client.on('ready', () => {
   console.log(`Logged in...`); // wea on bot ready
 });
 
+client.on('')
+
 client.on('message', msg => {
+
+    if (message.author.BOT) return;
+
   msg.reply('pong');
 });
 
